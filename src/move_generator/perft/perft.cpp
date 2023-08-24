375cbc963f7a6f149027d5b147f3ba8519e90296
Perry Jiang <me@perryjiang.com>
2023-08-24 11:12:46 -0500

    move_generator: disambiguate between parent and child contexts (#72)

--- src/move_generator/perft/perft.cpp
+++ src/move_generator/perft/perft.cpp
@@ -22,46 +22,47 @@ class Visitor : public move_generator::Visitor<Visitor<Derived, SubVisitor>> {
  public:
   constexpr explicit Visitor(Node& node, const Ply depth) noexcept : node_(node), depth_(depth) {}
 
-  template <Node::Context context>
+  template <Node::Context child_context>
   constexpr void visit_pawn_move(const auto& move) const noexcept {
-    visit<context>(move);
+    visit<child_context>(move);
   }
 
-  template <Node::Context context>
+  template <Node::Context child_context>
   constexpr void visit_knight_move(const auto& move) const noexcept {
-    visit<context>(move);
+    visit<child_context>(move);
   }
 
-  template <Node::Context context>
+  template <Node::Context child_context>
   constexpr void visit_bishop_move(const auto& move) const noexcept {
-    visit<context>(move);
+    visit<child_context>(move);
   }
 
-  template <Node::Context context>
+  template <Node::Context child_context>
   constexpr void visit_rook_move(const auto& move) const noexcept {
-    visit<context>(move);
+    visit<child_context>(move);
   }
 
-  template <Node::Context context>
+  template <Node::Context child_context>
   constexpr void visit_queen_move(const auto& move) const noexcept {
-    visit<context>(move);
+    visit<child_context>(move);
   }
 
-  template <Node::Context context>
+  template <Node::Context child_context>
   constexpr void visit_king_move(const auto& move) const noexcept {
-    visit<context>(move);
+    visit<child_context>(move);
   }
 
   constexpr void is_check() const noexcept {}
 
  private:
-  template <Node::Context context>
+  template <Node::Context child_context>
   constexpr void visit(const auto& move) const noexcept {
     if (auto& leaf_node_count = static_cast<const Derived&>(*this).leaf_node_count(move); depth_ == Ply{0}) {
       ++leaf_node_count;
     } else {
-      const auto undo = this->template scoped_move<!context.side_to_move, context.can_en_passant>(node_, move);
-      walk<context>(node_, SubVisitor(node_, decrement(depth_), leaf_node_count));
+      const auto undo =
+          this->template scoped_move<!child_context.side_to_move, child_context.can_en_passant>(node_, move);
+      walk<child_context>(node_, SubVisitor(node_, decrement(depth_), leaf_node_count));
     }
   }
 
